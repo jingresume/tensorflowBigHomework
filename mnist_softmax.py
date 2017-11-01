5bbf078ce81872ea222f63061c547c4c98586eea
shejing <2016192403@qq.com>
2017-11-01 19:50:20 +0800

    modified mnist_softmax.py

--- mnist_softmax.py
+++ mnist_softmax.py
@@ -11,7 +11,7 @@ b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x,W)+b)
 y_ = tf.placeholder("float", [None,10] )
 cross_entropy = -tf.reduce_sum(y_*tf.log(y))
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.0001).minimize(cross_entropy)
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
@@ -19,6 +19,8 @@ sess.run(init)
 for i in range(10000):
      batch_xs,batch_ys = mnist.train.next_batch(100)
      sess.run(train_step,feed_dict={x: batch_xs, y_: batch_ys})
+     if i % 1000 == 0 : 
+         print(sess.run(cross_entropy,feed_dict={x: batch_xs, y_: batch_ys}))
 
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
